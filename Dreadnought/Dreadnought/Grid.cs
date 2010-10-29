3d09e7a903c22415727be1117b78f657e5f364c1
nurbs666 <nurbs999@gmx.de>
2010-10-30 01:46:18 +0200

    grid changes to be cubes; maybe rename to cube.cs??

--- Dreadnought/Dreadnought/Grid.cs
+++ Dreadnought/Dreadnought/Grid.cs
@@ -8,27 +8,28 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Dreadnought {
     class Grid :Microsoft.Xna.Framework.DrawableGameComponent {
-        
+
         private BasicEffect effect;
         private List<VertexPositionColor> pointList;
         private List<short> pointOrder;
-		public Vector3 Position;
+        public Vector3 Position;
 
-        public Grid( Game game ) : base( game ) {
+        public Grid( Game game )
+            : base( game ) {
             pointList = new List<VertexPositionColor>();
             pointOrder = new List<short>();
 
             // fill list with vertices
             // dont try to put both inner for-llops into one
             // this would change the vertices order!
-            for( int z = 0; z <= 5; z++ ) {
+            for( int z = 0; z <= 20; z++ ) {
                 for( int i = 0; i <= 5; i++ ) {
                     pointList.Add( new VertexPositionColor( new Vector3( i, 0, z ), Color.DarkBlue ) );
-					pointList.Add(new VertexPositionColor(new Vector3(i, 5, z), Color.DarkBlue));
+                    pointList.Add( new VertexPositionColor( new Vector3( i, 5, z ), Color.DarkBlue ) );
                 }
                 for( int i = 0; i <= 5; i++ ) {
-					pointList.Add(new VertexPositionColor(new Vector3(0, i, z), Color.DarkBlue));
-					pointList.Add(new VertexPositionColor(new Vector3(5, i, z), Color.DarkBlue));
+                    pointList.Add( new VertexPositionColor( new Vector3( 0, i, z ), Color.DarkBlue ) );
+                    pointList.Add( new VertexPositionColor( new Vector3( 5, i, z ), Color.DarkBlue ) );
                 }
             }
 
@@ -36,16 +37,20 @@ namespace Dreadnought {
             for( short i = 0; i < pointList.Count; i++ ) {
                 pointOrder.Add( i );
             }
+            for( short i = 0; i < pointList.Count - 24; i++ ) {
+                pointOrder.Add( i );
+                pointOrder.Add( (short)( i + 24 ) );
+            }
         }
 
         protected override void LoadContent() {
             effect = new BasicEffect( Game.GraphicsDevice );
             effect.VertexColorEnabled = true;
             effect.LightingEnabled = false;
-				effect.FogEnabled = true;
-				effect.FogColor = new Vector3(0.0f);
-				effect.FogStart = 5000f;
-				effect.FogEnd = 15000f;
+            effect.FogEnabled = true;
+            effect.FogColor = new Vector3( 0.0f );
+            effect.FogStart = 5000.0f;
+            effect.FogEnd = 15000.0f;
 
 
         }
@@ -53,14 +58,14 @@ namespace Dreadnought {
         public override void Update( GameTime gameTime ) {
             effect.View = ( (Game)Game ).Camera.View;
             effect.Projection = ( (Game)Game ).Camera.Projection;
-            effect.World = Matrix.CreateWorld( Vector3.Zero, Vector3.Forward, Vector3.Up )* Matrix.CreateTranslation(Position) * Matrix.CreateScale( 5000.0f );
+            effect.World = Matrix.CreateWorld( Vector3.Zero, Vector3.Forward, Vector3.Up ) * Matrix.CreateTranslation( Position ) * Matrix.CreateScale( 5000.0f );
             base.Update( gameTime );
         }
 
         public override void Draw( GameTime gameTime ) {
             foreach( EffectPass pass in effect.CurrentTechnique.Passes ) {
                 pass.Apply();
-                GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>( 
+                GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>(
                     PrimitiveType.LineList,
                     pointList.ToArray(),
                     0,
