efa07134027e3191596cdf63ce0ecdb080bb85ef
Killili <jan@himmelmann.me>
2010-11-03 21:54:34 +0100

    -Git Ignore Update
    -Config update
    -Collection Extensions

---
+++ Dreadnought/Dreadnought/Common/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dreadnought.Common {
+	public static class Extensions {
+		public static void Each<T>(this IEnumerable<T> source, Action<T> action) {
+			foreach(T item in source)
+				action(item);
+		}
+
+		public static void Map<TKey,TValue>(this IDictionary<TKey,TValue> source, Func<TKey,TValue,TValue> function) {
+			foreach(KeyValuePair<TKey,TValue> item in source) {
+				source[item.Key] = function(item.Key,item.Value);
+			}
+		}
+		public static void Each<TKey, TValue>(this IDictionary<TKey, TValue> source, Action<TKey, TValue> action) {
+			foreach(KeyValuePair<TKey, TValue> item in source) {
+				action(item.Key, item.Value);
+			}
+		}
+	}
+}
